2f7fa1948b68c10ccb5cfa7ca521b746a5d11869
Sowndaryakp <sonalshetty.1999@gmail.com>
2023-10-25 16:33:51 +0530

    headers

--- src/components/welder.vue
+++ src/components/welder.vue
@@ -18,6 +18,7 @@
               <th class="px-4 py-2 font-poppins">Live</th>
               <th class="px-4 py-2 font-poppins">Project</th>
               <th class="px-4 py-2 font-poppins">Remarks</th>
+              //headers
             </tr>
           </thead> 
           <!-- flex space-y-2 flex-col items-center -->
